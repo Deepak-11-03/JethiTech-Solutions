965622a9ce2c0a0827018724ba4b70c71cda3c96
Deepak-11-03 <you@example.com>
2023-04-04 22:10:19 +0530

    completed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,25 +1,30 @@
 import React from "react";
 import "./style.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from '@mui/icons-material/Close';
+import { useState } from "react";
 
 export default function Header() {
+  const [sidebar,setSidebar] = useState(false)
+ 
   return (
-   <>
-     <header>
-      <div className="logo">
-      <img src="./logo.jpg" alt="website logo" />
-      </div>
-      <ul className="nav">
-        <li>Home</li>
-        <li>Our Offerings</li>
-        <li>Our Team</li>
-        <li>Contact Us</li>
-        <li>Blogs</li>
-      </ul>
-      <div className="button">
-        <button>Register Now</button>
+    <header>
+      <span className="menuButton" onClick={()=>setSidebar(!sidebar)}>{sidebar ? <CloseIcon/> :<MenuIcon/>} </span>
+      <div className="header">
+        <div className="logo">
+          <img src="./logo.jpg" alt="website logo" />
+        </div>
+        <ul className="nav" style={{display:!sidebar && "none"}}>
+          <li>Home</li>
+          <li>Our Offerings</li>
+          <li>Our Team</li>
+          <li>Contact Us</li>
+          <li>Blogs</li>
+        </ul>
+        <div className="button">
+          <button>Register Now</button>
+        </div>
       </div>
     </header>
-    <hr />
-   </>
   );
 }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -14,7 +14,7 @@ function Main() {
         <div id="shape"><h1>Making Your <span style={{color:"#FAFF00"}}>Real Estate</span> Purchase Journey Faster And Transparent</h1></div>
       </div>
       <div className="section2">
-        <img src="./housekey.jpg" alt="house" style={{width:"40%"}}/>
+        <img src="./housekey.jpg" alt="house" />
         <div>
           <h2>Who We Are</h2>
           <p>The Indian real estate sector is resilient despite global headwinds. Returns are impressive and the sector is expected to reach US$ 1 trillion in market size by 2030 (Niti Aayog report, 2021). However, the whole real estate purchase journey is very tedious and takes more than 2 years on average. Further, as the ticket size of this investment is large, security and trust become quite critical. Problems compound when you are currently away from your purchase destination. If you are experiencing difficulties in the real estate purchase journey, we are here to make it fast and transparent.</p> 
